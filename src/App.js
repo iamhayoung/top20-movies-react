d4251875d31d82ef89ac9a5b2e597da10f108678
Hayoung Gam <hahhyoung@gmail.com>
2021-03-18 03:35:28 +0900

    Counter

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ class App extends React.Component {
   componentDidMount() {
     console.log('componentdidmount')
   }
+  state = {
+    number: 0
+  }
+  handleIncrease = () => {
+    const { number } = this.state;
+    this.setState({
+      number: number + 1
+    })
+  }
+  handleDecrease = () => {
+    this.setState(({ number }) => ({
+      number: number - 1
+    }))
+  }
   render() {
     console.log('render')
     return (
       <>
         <GlobalStyle />
+        <h1>카운터</h1>
+        <div>값: {this.state.number}</div>
+        <button onClick={this.handleIncrease}>+</button>
+        <button onClick={this.handleDecrease}>-</button>
         <Movie />
       </>
     )
